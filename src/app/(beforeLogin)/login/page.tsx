8fb0250899a68b1fc0e28e8ea7f0c855fbddaa2c
ihj0906 <hojun7291@naver.com>
2024-02-20 02:03:54 +0900

    login modal update

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,9 +1,19 @@
-import styles from '../page.module.css';
+'use client';
+
+import { useRouter } from 'next/navigation';
+import styles from '@/app/page.module.css';
 import Image from 'next/image';
 import Link from 'next/link';
-import zLogo from '../../../public/zlogo.png';
+import zLogo from '../../../../public/zlogo.png';
 
-export default function Home() {
+export default function Login() {
+    const router = useRouter();
+    // 뒤로 가기 시의 차이점 존재
+    // push localhost:3001 -> localhost:3001/login -> localhost:3001/i/flow/login
+    // router.push('i/flow/login');
+    // replace localhost:3001 -> localhost:3001/i/flow/login
+    router.replace('/i/flow/login');
+
     return (
         <>
             <div className={styles.left}>
