056ba5bde0740f510c2027c61394fb2b4517b090
ihj0906 <hojun7291@naver.com>
2024-02-20 14:10:28 +0900

    main, login page 공통 컴포넌트 적용

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,10 +1,7 @@
 'use client';
 
+import Main from '@/app/(beforeLogin)/_component/Main';
 import { useRouter } from 'next/navigation';
-import styles from '@/app/page.module.css';
-import Image from 'next/image';
-import Link from 'next/link';
-import zLogo from '../../../../public/zlogo.png';
 
 export default function Login() {
     const router = useRouter();
@@ -14,22 +11,5 @@ export default function Login() {
     // replace localhost:3001 -> localhost:3001/i/flow/login
     router.replace('/i/flow/login');
 
-    return (
-        <>
-            <div className={styles.left}>
-                <Image src={zLogo} alt="logo" />
-            </div>
-            <div className={styles.right}>
-                <h1>지금 일어나고 있는 일</h1>
-                <h2>지금 가입하세요.</h2>
-                <Link href="/i/flow/signup" className={styles.signup}>
-                    계정 만들기
-                </Link>
-                <h3>이미 트위터에가입하셨나요?</h3>
-                <Link href="/login" className={styles.login}>
-                    로그인
-                </Link>
-            </div>
-        </>
-    );
+    return <Main />;
 }
