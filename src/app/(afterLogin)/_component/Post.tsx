a14fc7288cdfefa9698ba9029de6121d5545a87c
ihj0906 <hojun7291@naver.com>
2024-03-19 03:06:01 +0900

    post images update

--- src/app/(afterLogin)/_component/Post.tsx
+++ src/app/(afterLogin)/_component/Post.tsx
@@ -6,6 +6,7 @@ import 'dayjs/locale/ko';
 import ActionButtons from '@/app/(afterLogin)/_component/ActionButtons';
 import PostArticle from './PostArticle';
 import { faker } from '@faker-js/faker';
+import PostImages from '@/app/(afterLogin)/_component/PostImages';
 
 dayjs.locale('ko');
 dayjs.extend(relativeTime);
@@ -21,16 +22,30 @@ export default function Post({ noImage }: Props) {
             nickname: '치지직',
             image: '/unnamed.png',
         },
-        content: '클론코딩 라이브로 하니 너무 힘들어요 ㅠㅠ',
+        content: 'X 됐다',
         createdAt: new Date(),
         Images: [] as any[],
     };
     // 50% 확률
     if (Math.random() > 0.5 && !noImage) {
-        target.Images.push({
-            imageId: 1,
-            link: faker.image.urlLoremFlickr(),
-        });
+        target.Images.push(
+            {
+                imageId: 1,
+                link: faker.image.urlLoremFlickr(),
+            },
+            {
+                imageId: 2,
+                link: faker.image.urlLoremFlickr(),
+            },
+            {
+                imageId: 3,
+                link: faker.image.urlLoremFlickr(),
+            },
+            {
+                imageId: 4,
+                link: faker.image.urlLoremFlickr(),
+            }
+        );
     }
 
     return (
@@ -65,15 +80,8 @@ export default function Post({ noImage }: Props) {
                         </span>
                     </div>
                     <div>{target.content}</div>
-                    <div className={style.postImageSection}>
-                        {target.Images && target.Images.length > 0 && (
-                            <Link
-                                href={`/${target.User.id}/status/${target.postId}/photo/${target.Images[0].imageId}`}
-                                className={style.postImageSection}
-                            >
-                                <img src={target.Images[0]?.link} alt="" />
-                            </Link>
-                        )}
+                    <div>
+                        <PostImages post={target} />
                     </div>
                     <ActionButtons />
                 </div>
